e9b154012da8673216bfb9d6a5bb0526ae855b92
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-11-20 14:22:58 +0530

    tested wihtout group classes

---
+++ w6/Test.java
@@ -0,0 +1,35 @@
+package w6;
+
+import java.util.LinkedList;
+
+public class Test {  
+
+    public static void main(String[] args) {
+        new GladiatorPrimeTest().rent(); 
+    }  
+}
+
+
+class GladiatorPrimeTest
+{
+    private LinkedList<StreamedMovie> movies = new LinkedList<StreamedMovie>();
+    private Customer customer; 
+
+    GladiatorPrimeTest() {
+        customer = new Customer(1, "Jack");
+        movies.add(new StreamedMovie(1, "Harry Potter 1", 100.0, 7, Status.FOR_RENT));
+        movies.add(new StreamedMovie(1, "Jurrasic Park", 90.0, 7, Status.UNAVAILABLE));
+    }
+
+    public void rent()
+    {
+        for (StreamedMovie streamedMovie : movies) {
+            rentMovie(streamedMovie);
+        }
+    }
+
+    private void rentMovie(StreamedMovie movie)
+    {
+        customer.rent(movie);
+    }
+}
