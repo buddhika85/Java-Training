b5f953d8ac1b88c7f1d2372e295eb7c6afdec05b
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2024-04-17 19:49:30 +0530

    java testing - dictionaries

--- w7-p1/WarmUp.java
+++ w7-p1/WarmUp.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class WarmUp 
 {
@@ -13,5 +14,26 @@ public class WarmUp
         System.out.println(numbers);
         ArrayList<Integer> hundreads = new ArrayList<>(Arrays.asList(100, 200, 300, 400));
         System.out.println(hundreads);
+
+        HashMap<Integer, Point> points = new HashMap<>();
+        points.put(0, new Point(0, 0));
+        points.put(1, new Point(1, 1));
+        System.out.println(points);
     }    
+}
+
+
+class Point
+{
+    int x, y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "Point [x=" + x + ", y=" + y + "]";
+    }
 }
