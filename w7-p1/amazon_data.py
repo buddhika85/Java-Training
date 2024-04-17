1630b2202a301c7ffbdc41ebc3ea44fede63d52b
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2024-04-17 21:26:45 +0530

    python set ops

--- w7-p1/amazon_data.py
+++ w7-p1/amazon_data.py
@@ -20,3 +20,22 @@ amazon_data = [
     {"id": 118, "is_prime": False, "monthly_spend": 1400.0},
     {"id": 119, "is_prime": True, "monthly_spend": 900.0}
 ]
+
+prime_set = set()
+non_prime_set = set()
+gt_2000 = set()
+between_1000_2000 = set()
+lt_1000 = set()
+
+for customer in amazon_data:
+    if customer["is_prime"]:
+        prime_set.add(customer["id"])
+    else:
+        non_prime_set.add(customer["id"])
+    if customer["monthly_spend"] > 2000:
+        gt_2000.add(customer["id"])
+        
+# for customer in prime_set:
+#     print(customer)
+
+print(f'There are {len(prime_set & gt_2000)} Prime members who spend > $2000')
