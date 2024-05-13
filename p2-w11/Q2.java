0ec5f4b4a7e7cc31a9330b7cfcf52a99c6ae0ac6
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2024-05-13 18:40:03 +0530

    q2

--- p2-w11/Q2.java
+++ p2-w11/Q2.java
@@ -1,3 +1,28 @@
-public class Q2 {
-    
+public class Q2 
+{
+    public static void main(String[] args) 
+    {
+        Bundle bundle = new Bundle(Phone.SAMSUNG, PhoneAccessory.HEADPHONES);
+        System.out.println(bundle.discountedPrice(0.1));            // 1250 * .9 = 1125
+    }    
+}
+
+
+
+class Bundle
+{
+	private Phone phone;
+	private PhoneAccessory phoneAccessory;
+
+	public Bundle(Phone phone, PhoneAccessory phoneAccessory)
+	{
+		this.phone = phone;
+		this.phoneAccessory = phoneAccessory;
+	}
+
+	// computes the discounted total price of the phone and accessory
+	public double discountedPrice(double discountPercentage)
+	{
+		return (phone.getPrice() + phoneAccessory.getPrice()) * (1 - discountPercentage);
+	}
 }
