e56bcbf817622daf2bfb425c1c3bcff599675d60
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2024-07-05 11:07:13 +1000

    p1-w3

---
+++ p1-w3-s/RandomGen.java
@@ -0,0 +1,15 @@
+public class RandomGen
+{
+    public static void main(String[] args) {
+        int seed = 34;
+        int a = 12;
+        int m = 100;
+        int c = 27;
+
+        int count = 0;
+        while(count < 20)
+        {
+            
+        }
+    }
+}
