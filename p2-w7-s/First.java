e9d08337083ab731c051b1fa4a1c4654e94e60b1
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2024-07-29 02:18:19 +1000

    w7 p2 start

--- p2-w7-s/First.java
+++ p2-w7-s/First.java
@@ -6,26 +6,49 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-public class First extends Application 
-{
+// The top-level class of a JavaFX app must extend the `Application` class.
+public class First extends Application {
     public static void main(String[] args) {
-        launch(args);
+        // To launch the app, we call the `launch` method, which is static and
+        // defined in the `Application` class.
+        First.launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) {
+        // We can set the title of the stage (window) to be whatever we want.
         primaryStage.setTitle("Hello! Bye!");
+
+        // We have 2 visible nodes in this app; a Label is used for displaying
+        // text, and a Button is for...clicking. They will be arranged vertically
+        // in the scene as children nodes of the VBox element, below.
 
         Label label = new Label("Click the button!");
 
         Button helloButton = new Button();
         helloButton.setText("Say 'Hello!'");
 
+        // The VBox is a node that arranges its children nodes in a vertical
+        // column from top to bottom. We call the variable `root` because it
+        // will be the root node of the scene graph (see below).
         VBox root = new VBox();
+
+        // Here we set `label` and `helloButton` as children nodes of the VBox.
+        // Note how `label` comes before `helloButton`.
         root.getChildren().addAll(label, helloButton);
+
+        // This method call aligns the VBox to sit right in the centre of the
+        // scene.
         root.setAlignment(Pos.CENTER);
 
-        primaryStage.setScene(new Scene(root, 300, 250));
+        // Scene is defined here with the VBox as the root node, a width of
+        // 300 pixels and height of 250 pixels.
+        Scene scene = new Scene(root, 300, 250);
+
+        // Set the scene within the primary stage
+        primaryStage.setScene(scene);
+
+        // Display the stage (window)
         primaryStage.show();
     }
 }
