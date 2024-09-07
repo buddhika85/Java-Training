f5b91b550ce376ffa8064a12ced7a901c5b811a8
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2024-09-08 07:56:19 +1000

    changes

---
+++ p1-w12/tenant.py
@@ -0,0 +1,10 @@
+class Tenant:
+    def __init__(self, name, industry, level, floot_location) -> None:
+        self.name = name
+        self.industry = industry
+        self.level = level
+        self.floor_location = floot_location
+        
+class OfficeBuilding:
+    def __init__(self) -> None:
+        pass
