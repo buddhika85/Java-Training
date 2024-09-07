218ea4cdaf916963a4917682ca9a5b17d023d9d1
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2024-09-08 08:03:27 +1000

    changes

--- p1-w12/tenant.py
+++ p1-w12/tenant.py
@@ -6,5 +6,19 @@ class Tenant:
         self.floor_location = floot_location
         
 class OfficeBuilding:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, tenants) -> None:
+        self.tenants = tenants                  # list of tenants
+    
+    def number_of_tenants(self):
+        return len(self.tenants)
+    
+    def number_of_levels(self):
+        levels_set = set()
+        for tenant in self.tenants:
+            levels_set.add(tenant.level)
+        return len(levels_set)
+    
+    def print_tenants(self, industry):
+        for tenant in self.tenants:
+            if tenant.industry == industry:
+                print(f'{tenant.name} => industry {tenant.industry}')
