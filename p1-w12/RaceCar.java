6c8b9532adc16002b029e4d26e7081119136ee30
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2024-09-09 09:15:16 +1000

    changes

---
+++ p1-w12/RaceCar.java
@@ -0,0 +1,14 @@
+public class RaceCar
+{
+    private double topSpeed;
+
+    RaceCar(double topSpeed)
+    {
+        this.topSpeed = topSpeed;
+    }
+
+    double getTopSpeed()
+    {
+        return this.topSpeed;
+    }
+}
