d30e584e70055d5a86de2752880093502b5d2f38
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2024-09-09 09:26:04 +1000

    changes

--- p1-w12/RaceCar.java
+++ p1-w12/RaceCar.java
@@ -12,3 +12,44 @@ public class RaceCar
         return this.topSpeed;
     }
 }
+
+class Scorcher extends RaceCar
+{
+    private boolean boostActive;
+
+    public Scorcher(double topSpeed) {
+        super(topSpeed);
+        this.boostActive = false;
+    }
+
+    void activateBoost()
+    {
+        this.boostActive = true;
+    }
+
+    @Override
+    double getTopSpeed()
+    {
+        if (boostActive)
+            return super.getTopSpeed() + 100;
+        return super.getTopSpeed();
+    }
+
+    void testDrive()
+    {
+        System.out.println("Current top speed is " + getTopSpeed());
+        System.out.println("Activate bost? (1 for yes, 2 for no)");
+        if (In.nextInt() == 1)
+        {
+            activateBoost();
+        }
+        System.out.println("Top speed now is " + getTopSpeed());
+    }
+}
+
+class Test
+{
+    public static void main(String[] args) {
+        new Scorcher(100).testDrive();
+    }
+}
