f6ded6e43b39653ea8cda3c6ccf045487eff9214
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2024-10-03 03:01:16 +1000

    w1-p1 tests

---
+++ 2024_Sep/P1/w1/Test.java
@@ -0,0 +1,18 @@
+class Test
+{
+    public static void main(String[] args) 
+    {
+        String pokemon = "Pikachu";
+
+        int wins = 20;
+        int losses = 3;
+
+        System.out.println(pokemon + "'s record:");
+        System.out.println("  - " + wins + " wins");
+        System.out.println("  - " + losses + " losses");
+        System.out.println("  - win-loss ratio of " + (wins / losses));
+
+        double d = 1.1;
+        System.out.println(wins + d);
+    }
+}
