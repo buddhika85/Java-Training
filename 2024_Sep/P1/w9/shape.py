af16fc07ae774d50bb29aefb18fa833a31c58ef9
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2024-11-30 23:56:25 +1100

    changes

---
+++ 2024_Sep/P1/w9/shape.py
@@ -0,0 +1,12 @@
+import math
+
+
+class Shape:
+    def __init__(self, name) -> None:
+        self.name = name
+    
+    def getPerimeter(self):
+        return 0.0
+    
+    def getArea(self):
+        return 0.0
