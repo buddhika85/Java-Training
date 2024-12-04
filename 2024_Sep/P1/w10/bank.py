955c52b6f3ae7786e9d2dee6040e0dbd33987b21
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2024-12-05 05:49:31 +1100

    changes

--- 2024_Sep/P1/w10/bank.py
+++ 2024_Sep/P1/w10/bank.py
@@ -19,20 +19,52 @@ class BankAccount:
         
     def __str__(self) -> str:
         return f'Bank account name {self.name} and balance {self.balance}'
+
+class Bank:
+    def __init__(self) -> None:
+        self.accounts = {}
+
+    def add_account(self, name, balance):
+        self.accounts[name] = BankAccount(name, balance)
+    
+    def transfer(self, sender_name, recipient_name, amount):
+        if self.accounts[sender_name] != None and self.accounts[recipient_name] != None:            
+            self.accounts[sender_name].withdraw(amount)
+            self.accounts[recipient_name].deposit(amount)
+            print(f'Success - Transferred {amount} from {sender_name} to {recipient_name}')
+        else:
+            print('Error - sender/receivers bank accounts does not exist')
         
+    def __str__(self) -> str:
+        str = 'Bank Accounts list\n'
+        for account_name in self.accounts.keys():
+            str += f'\t{self.accounts[account_name]}\n'
+        return str
+        
+# Lab    
 # test code
 # account = BankAccount('Mick', 1000.0)
 # account.withdraw(500.0)
 # account.deposit(1500.0)
 # print(account)
 
+# try:
+#     # account = BankAccount('Mick', -1)
+    
+#     account = BankAccount('Mick', 1000)
+#     # account.deposit(-1)
+#     # account.withdraw(-1)
+#     # account.withdraw(1000.1)
+    
+# except ValueError as e:
+#     print(f'Error - {e}')
+
+# TB
 try:
-    # account = BankAccount('Mick', -1)
-    
-    account = BankAccount('Mick', 1000)
-    # account.deposit(-1)
-    # account.withdraw(-1)
-    # account.withdraw(1000.1)
-    
+    bank = Bank()
+    bank.add_account("Ellen", 800)  
+    bank.add_account("Sophie", 2000.0)  
+    bank.transfer('Ellen', 'Sophie', 800)
+    print(bank)
 except ValueError as e:
     print(f'Error - {e}')
